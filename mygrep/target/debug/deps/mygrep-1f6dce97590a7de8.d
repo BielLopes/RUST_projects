ca8e1bc833c317e86f2ad6f6a608e460dacbbf44
Gabriel Machado <gabriel.machado@cmbh.mg.gov.br>
2024-12-03 20:19:54 -0300

    add: reading the file

---
+++ mygrep/target/debug/deps/mygrep-1f6dce97590a7de8.d
@@ -0,0 +1,5 @@
+/home/Galoma/Repos/RUST_projects/mygrep/target/debug/deps/mygrep-1f6dce97590a7de8: src/main.rs
+
+/home/Galoma/Repos/RUST_projects/mygrep/target/debug/deps/mygrep-1f6dce97590a7de8.d: src/main.rs
+
+src/main.rs:
---
+++ mygrep/target/debug/mygrep.d
@@ -0,0 +1,1 @@
+/home/Galoma/Repos/RUST_projects/mygrep/target/debug/mygrep: /home/Galoma/Repos/RUST_projects/mygrep/src/main.rs
