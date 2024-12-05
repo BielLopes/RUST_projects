edb4cf0d096745b7271075ad9c183d9e3328b000
Gabriel Lopes Machado <gabriel.machado@cmbh.mg.gov.br>
2024-12-05 07:15:17 -0300

    remove: target folder

--- mygrep/target/debug/deps/mygrep-1f6dce97590a7de8.d
+++
@@ -1,5 +0,0 @@
-/home/Galoma/Repos/RUST_projects/mygrep/target/debug/deps/mygrep-1f6dce97590a7de8: src/main.rs
-
-/home/Galoma/Repos/RUST_projects/mygrep/target/debug/deps/mygrep-1f6dce97590a7de8.d: src/main.rs
-
-src/main.rs:
--- mygrep/target/debug/mygrep.d
+++
@@ -1,1 +0,0 @@
-/home/Galoma/Repos/RUST_projects/mygrep/target/debug/mygrep: /home/Galoma/Repos/RUST_projects/mygrep/src/main.rs
